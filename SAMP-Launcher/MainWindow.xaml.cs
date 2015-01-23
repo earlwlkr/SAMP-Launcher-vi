1b83838cf3099af60ab290e95a40006218518af7
Earl Walker <mike358015@gmail.com>
2015-01-23 09:19:54 +0700

    Initial commit

---
+++ SAMP-Launcher/MainWindow.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Net;
+
+namespace SAMP_Launcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string    _launcherTitle  = "SAMP Launcher";
+        // Tên miền hoặc địa chỉ IP của server.
+        private const string    _serverName     = "127.0.0.1";
+        private const int       _serverPort     = 7777;
+        // Password để vào server.
+        private const string    _serverPassword = "";
+
+        private string      _serverIP;
+        private string      _playerName;
+        private SAMP.Query  _query;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MainWindow1.Title = _launcherTitle;
+            lblSvrName.Content = _launcherTitle;
+            lblStatus.Text = "Đang chờ";
+        }
+
+        private void MainWindow1_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (checkFiles() == false)
+                Application.Current.Shutdown();
+
+            // Lấy tên nhân vật từ registry và cho vào textbox.
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\SAMP");
+            if (key != null)
+            {
+                Object value = key.GetValue("PlayerName");
+                if (value != null)
+                {
+                    txtbxPlayerName.Text = value.ToString();
+                }
+            }
+            _playerName = txtbxPlayerName.Text;
+
+
+            IPAddress svIP;
+            if (IPAddress.TryParse(_serverName, out svIP))
+            {
+                _serverIP = svIP.ToString();
+            }
+            else
+            {
+                // Phân giải tên miền.
+                IPHostEntry hostEntry = Dns.GetHostEntry(_serverName);
+                if (hostEntry.AddressList.Length == 0)
+                {
+                    lblStatus.Text = "<!> Đã có lỗi xảy ra với DNS. Xin vui lòng liên hệ Admin.";
+                    return;
+                }
+                _serverIP = hostEntry.AddressList[0].ToString();
+            }
+            _query = new SAMP.Query(_serverIP, _serverPort, false);
+
+            updateServerInfo();
+        }
+
+        private void updateServerInfo()
+        {
+            if (_query.Send((char)SAMP.Query.PaketOpcode.Info))
+            {
+                int count = _query.Receive();
+                if (count == 0)
+                {
+                    lblStatus.Text = "<!> Không thể kết nối đến server.";
+                    btnConnect.IsEnabled = false;
+                    return;
+                }
+
+                lblServer.Content = _query.hostname;
+                lblGameMode.Content = _query.gamemode;
+                lblPlayersCount.Content = _query.players + '/' + _query.max_players;
+
+                if (_query.Send((char)SAMP.Query.PaketOpcode.Rules))
+                {
+                    count = _query.Receive();
+                    if (count == 0)
+                    {
+                        lblStatus.Text = "<!> Không thể kết nối đến server.";
+                        btnConnect.IsEnabled = false;
+                        return;
+                    }
+                    string[] rules = _query.Store(count);
+
+                    for (int i = 0; i < rules.Length - 2; i += 2)
+                    {
+                        if (rules[i] == "mapname")
+                        {
+                            lblMap.Content = rules[i + 1];
+                        }
+                        else if (rules[i] == "weburl")
+                        {
+                            lblWebsite.Content = rules[i + 1];
+                        }
+                    }
+                }
+            }
+            else
+            {
+                btnConnect.IsEnabled = false;
+                lblStatus.Text = "<!> Không thể kết nối đến server.";
+            }
+        }
+
+        private bool checkFiles()
+        {
+            if (File.Exists("samp.exe") == false)
+            {
+                MessageBox.Show("<!> Không tìm thấy samp.exe\n<!> Vui lòng bỏ Launcher vào cùng thư mục với file samp.exe (trong folder game).", "Lỗi");
+                return false;
+            }
+            //if (File.Exists("d3d9.dll"))
+            //{
+            //    MessageBox.Show("<!> Hê thống nhận thấy bạn đang sử dụng phần mềm thứ 3 không hợp lệ (s0beit).\n<!> Hãy xóa các file đó hoặc re-install game.", "Lỗi");
+            //    return false;
+            //}
+            //if (File.Exists("cleo.asi"))
+            //{
+            //    MessageBox.Show("<!> Hê thống nhận thấy bạn đang sử dụng phần mềm thứ 3 không hợp lệ (CLEO).\n<!> Hãy xóa các file đó hoặc re-install game.", "Lỗi");
+            //    return false;
+            //}
+            return true;
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (checkFiles() == false)
+                return;
+     
+            // kiểm tra tên nhân vật có thay đổi không
+            lblStatus.Text = "[*] Đang kiểm tra tên nhân vật.";
+            if (txtbxPlayerName.Text != _playerName)
+            {
+                // nếu tên mới không thỏa điều kiện
+                if (txtbxPlayerName.Text.Length > 20 || txtbxPlayerName.Text.Length < 4)
+                {
+                    MessageBox.Show("<!> Tên không được ít hơn 4 hoặc nhiều hơn 20 ký tự.", "Tên không hợp lệ!");
+                    return;
+                }
+
+                // chỉnh lại giá trị trong registry
+                RegistryKey setKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\SAMP");
+                if (setKey != null)
+                {
+                    setKey.SetValue("PlayerName", txtbxPlayerName.Text);
+                }
+            }
+            //MessageBox.Show(_serverIP + ":" + _serverPort);
+
+            lblStatus.Text = "[*] Đang tiến hành kết nối đến server.";
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "samp.exe";
+            startInfo.Arguments = _serverIP + ":" + _serverPort + " " + _serverPassword;
+            lblStatus.Text = "[*] Đang vào game.";
+            Process.Start(startInfo);
+            lblStatus.Text = "[*] Thành công.";
+        }
+    }
+}
---
+++ SAMP-Launcher/SAMP.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+
+namespace SAMP
+{
+    public class SAMP : IDisposable
+    {
+        Socket qSocket;
+        IPAddress address;
+        int _port = 0;
+        string _password = null;
+        string[] results = new string[50];
+        int _count = 0;
+
+        public SAMP(string addr, int port, string password, bool dns)
+        {
+            qSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            qSocket.SendTimeout = 5000;
+            qSocket.ReceiveTimeout = 5000;
+            if (dns)
+            {
+                try
+                {
+                    address = Dns.GetHostAddresses(addr)[0];
+                }
+                catch
+                {
+                }
+            }
+            else
+            {
+                try
+                {
+                    address = IPAddress.Parse(addr);
+                }
+                catch
+                {
+                }
+            }
+
+            _port = port;
+            _password = password;
+        }
+
+        public bool Send(string command)
+        {
+            try
+            {
+                IPEndPoint endpoint = new IPEndPoint(address, _port);
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    using (BinaryWriter writer = new BinaryWriter(stream))
+                    {
+                        writer.Write("SAMP".ToCharArray());
+                        string[] SplitIP = address.ToString().Split('.');
+                        writer.Write(Convert.ToByte(SplitIP[0]));
+                        writer.Write(Convert.ToByte(SplitIP[1]));
+                        writer.Write(Convert.ToByte(SplitIP[2]));
+                        writer.Write(Convert.ToByte(SplitIP[3]));
+                        writer.Write((ushort)_port);
+                        writer.Write('x');
+                        writer.Write((ushort)_password.Length);
+                        writer.Write(_password.ToCharArray());
+                        writer.Write((ushort)command.Length);
+                        writer.Write(command.ToCharArray());
+                    }
+                    if (qSocket.SendTo(stream.ToArray(), endpoint) > 0) return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return false;
+        }
+
+        public int Receive()
+        {
+            try
+            {
+                for (int i = 0; i < results.GetLength(0); i++) results.SetValue(null, i);
+                _count = 0;
+                EndPoint endpoint = new IPEndPoint(address, _port);
+                byte[] rBuffer = new byte[500];
+                int count = qSocket.ReceiveFrom(rBuffer, ref endpoint);
+                using (MemoryStream stream = new MemoryStream(rBuffer))
+                {
+                    using (BinaryReader reader = new BinaryReader(stream))
+                    {
+                        if (stream.Length <= 11) return _count;
+                        reader.ReadBytes(11);
+                        short len;
+                        try
+                        {
+                            while ((len = reader.ReadInt16()) != 0) results[_count++] = new string(reader.ReadChars((int)len));
+                        }
+                        catch
+                        {
+                            return _count;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return _count;
+            }
+            return _count;
+        }
+
+        public string[] Store(int count = -1)
+        {
+            string[] rString = new string[count != -1 ? count : _count];
+            for (int i = 0; (i < count || count == -1) && i < _count; i++) rString[i] = results[i];
+            _count = 0;
+            return rString;
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                qSocket.Dispose();
+            }
+            catch
+            {
+            }
+        }
+    }
+
+    public class Query : IDisposable
+    {
+        Socket qSocket;
+        IPAddress address;
+        int _port = 0;
+        string[] results;
+        int _count = 0;
+        DateTime[] timestamp = new DateTime[2];
+
+        public string passworded;
+        public string players;
+        public string max_players;
+        public string hostname;
+        public string gamemode;
+        public string mapname;
+
+        public enum PaketOpcode
+        {
+            Info = 'i',
+            Rules = 'r',
+            ClientList = 'c',
+            DetailedClientList = 'd',
+            Ping = 'p'
+        }
+
+        public Query(string addr, int port, bool dns)
+        {
+            qSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            qSocket.SendTimeout = 5000;
+            qSocket.ReceiveTimeout = 5000;
+            if (dns)
+            {
+                try
+                {
+                    address = Dns.GetHostAddresses(addr)[0];
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("An error has occured in SAMP API!\n\n" + e.ToString());
+                    Environment.Exit(2);
+                }
+            }
+            else
+            {
+                try
+                {
+                    address = IPAddress.Parse(addr);
+                }
+                catch
+                {
+                }
+            }
+            _port = port;
+        }
+
+        public bool Send(char opcode, string sign = "1337")
+        {
+            try
+            {
+                EndPoint endpoint = new IPEndPoint(address, _port);
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    using (BinaryWriter writer = new BinaryWriter(stream))
+                    {
+                        writer.Write("SAMP".ToCharArray());
+                        string[] SplitIP = address.ToString().Split('.');
+                        writer.Write(Convert.ToByte(SplitIP[0]));
+                        writer.Write(Convert.ToByte(SplitIP[1]));
+                        writer.Write(Convert.ToByte(SplitIP[2]));
+                        writer.Write(Convert.ToByte(SplitIP[3]));
+                        writer.Write((ushort)_port);
+                        writer.Write(opcode);
+                        if (opcode == 'p') writer.Write(sign.ToCharArray());
+                        timestamp[0] = DateTime.Now;
+                    }
+                    if (qSocket.SendTo(stream.ToArray(), endpoint) > 0) return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return false;
+        }
+
+        public int Receive()
+        {
+            try
+            {
+                _count = 0;
+                EndPoint endpoint = new IPEndPoint(address, _port);
+                byte[] rBuffer = new byte[500];
+                qSocket.ReceiveFrom(rBuffer, ref endpoint);
+                timestamp[1] = DateTime.Now;
+                using (MemoryStream stream = new MemoryStream(rBuffer))
+                {
+                    using (BinaryReader reader = new BinaryReader(stream))
+                    {
+                        if (stream.Length <= 10) return _count;
+                        reader.ReadBytes(10);
+                        switch (reader.ReadChar())
+                        {
+                            case 'i':
+                                {
+                                    results = new string[6];
+                                    passworded = reader.ReadByte().ToString();
+                                    //results[_count++] = reader.ReadByte().ToString(); // either 0 or 1, depending whether if the password has been set.
+                                    players = reader.ReadInt16().ToString();
+                                    //results[_count++] = reader.ReadInt16().ToString(); // current amount of players online on the server
+                                    max_players = reader.ReadInt16().ToString();
+                                    //results[_count++] = reader.ReadInt16().ToString(); // maximum amount of players that can join the server
+                                    hostname = new string(reader.ReadChars(reader.ReadInt32()));
+                                    //results[_count++] = new string(reader.ReadChars(reader.ReadInt32())); // hostname
+                                    gamemode = new string(reader.ReadChars(reader.ReadInt32()));
+                                    //results[_count++] = new string(reader.ReadChars(reader.ReadInt32())); // gamemode
+                                    mapname = new string(reader.ReadChars(reader.ReadInt32()));
+                                    //results[_count++] = new string(reader.ReadChars(reader.ReadInt32())); // mapname
+                                    return 6;
+                                }
+
+                            case 'r':
+                                {
+                                    int rulecount = reader.ReadInt16();
+                                    results = new string[rulecount * 2];
+                                    for (int i = 0; i < rulecount; i++)
+                                    {
+                                        results[_count++] = new string(reader.ReadChars(reader.ReadByte())); // rule name (key)
+                                        results[_count++] = new string(reader.ReadChars(reader.ReadByte())); // rule value (value)
+                                    }
+                                    return _count;
+                                }
+
+                            case 'c':
+                                {
+                                    int playercount = reader.ReadInt16();
+                                    results = new string[playercount * 2];
+                                    for (int i = 0; i < playercount; i++)
+                                    {
+                                        results[_count++] = new string(reader.ReadChars(reader.ReadByte())); // nickname
+                                        results[_count++] = reader.ReadInt32().ToString(); // score
+                                    }
+                                    return _count;
+                                }
+
+                            case 'd':
+                                {
+                                    int playercount = reader.ReadInt16();
+                                    results = new string[playercount * 4];
+                                    for (int i = 0; i < playercount; i++)
+                                    {
+                                        results[_count++] = reader.ReadByte().ToString(); //playerid
+                                        results[_count++] = new string(reader.ReadChars(reader.ReadByte())); //nick
+                                        results[_count++] = reader.ReadInt32().ToString(); //score
+                                        results[_count++] = reader.ReadInt32().ToString(); //ping
+                                    }
+                                    return _count;
+                                }
+
+                            case 'p':
+                                {
+                                    results = new string[1];
+                                    results[_count++] = timestamp[1].Subtract(timestamp[0]).Milliseconds.ToString(); // time difference
+                                    results[_count++] = new string(reader.ReadChars(4)); // paket signature
+                                    return _count;
+                                }
+
+                            default: return _count;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return _count;
+            }
+        }
+
+        public string[] Store(int count)
+        {
+            string[] rString = new string[count];
+            for (int i = 0; i < count && i < _count; i++) rString[i] = results[i];
+            _count = 0;
+            return rString;
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                qSocket.Dispose();
+            }
+            catch
+            {
+            }
+        }
+    }
+}
